01c9355e0238cc26c9cb7b77ab3de0335ec3bd34
bannadas <bannadaso39@gmail.com>
2021-09-02 17:57:10 +0600

    some small changes

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const searchResult = document.getElementById('search-result');
 const errorDiv = document.getElementById('error');
 const resultNumber = document.getElementById('result-number');
 
-
 const searchBook = () => {
     const searchText = searchInput.value;
 //    clear input field
@@ -15,7 +14,6 @@ const searchBook = () => {
     .then(res => res.json())
     .then(data => displaySearchResult(data.docs));  
 }
-
 //  function for displaying book
 const displaySearchResult = books => {
 
@@ -23,7 +21,6 @@ const displaySearchResult = books => {
     const showResultNumber = () => { 
         resultNumber.innerText =books.length + ' results found';  
     };
-
     resultNumber.innerText = "";
 
     // error handling
@@ -43,17 +40,16 @@ const displaySearchResult = books => {
         const div = document.createElement('div');
         div.classList.add('col-md-3');
         div.innerHTML = `
-                <div class="card h-100">
+             <div class="card h-100">
                   <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid" alt="">
-                  <div class="card-body">
+                 <div class="card-body">
                       <h5 class="card-title" >Book Title: ${book.title}</h5>
-                      <h6 class="card-text"> Author name: ${book.author_name}</h6>
+                      <p class="card-text"> Author name: ${book.author_name}</p>
                       <p class="card-text"> Publisher Name: ${book.publisher}</p>
                       <p class="card-text"> Publish Year: ${book.first_publish_year}</p>
                       
                   </div>
-              </div>
-        `;
+              </div> `;
         searchResult.appendChild(div);
     });
 
